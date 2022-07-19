5e15ec4257f63384c0308387b37ea8cd1bccbffc
Valcan-Andrada <andrada.valcan01@e-uvt.ro>
2022-07-19 16:07:16 +0300

    .

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,14 +1,21 @@
+import org.apache.hadoop.shaded.org.apache.avro.generic.GenericData;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import static org.apache.spark.sql.functions.col;
+import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.types.DataTypes;
+import scala.Array;
+
+
+import static org.apache.spark.sql.functions.*;
 
 public class Start {
     public static void main(String[] args) throws AnalysisException {
-        
+
         System.setProperty("hadoop.home.dir","C:\\BigData\\Hadoop\\bin");
 
         SparkSession spark = SparkSession
@@ -48,6 +55,9 @@ public class Start {
 //                        .groupBy("Receiving Country Code")
 //                        .count().show();
 
+        //filtrare 3 coduri de tara care a primit studenti si care au fost codurile
+        //de tari din care au provenit studentii si care a fost numarul lor
+        //+afisare sortata
         os.filter((os.col("Receiving Country Code").equalTo("RO"))
                         .or(os.col("Receiving Country Code").equalTo("LV"))
                         .or(os.col("Receiving Country Code").equalTo("AT")))
@@ -55,6 +65,30 @@ public class Start {
                         .count().sort("Receiving Country Code","Sending Country Code").show(100);
 
 
+//**********************************
 
+        UDF1<Array<String>,String> groupData = new UDF1<Array<String> ,String>() {
+            @Override
+            public String call(Array<String> s) throws Exception {
+                System.out.println("++++++++++++++*+*+*+**+***********************************************************************");
+                //return String.valueOf(s.length());
+                return "da";
+            }
+        };
+
+
+
+        spark.udf().register("groupData",groupData, DataTypes.StringType);
+
+       os.filter((os.col("Receiving Country Code").equalTo("RO"))
+                        .or(os.col("Receiving Country Code").equalTo("LV"))
+                        .or(os.col("Receiving Country Code").equalTo("AT")))
+                .groupBy(col("Receiving Country Code")).agg(collect_set("Sending Country Code")
+                        .as("Sending Country Code")).sort("Sending Country Code").withColumn("groupData",callUDF("groupData",col("Sending Country Code"))).show(100);
+              // .count().sort("Receiving Country Code","Sending Country Code").show(100);
+
+//spark.udf().register("groupData",groupData, DataTypes.StringType);
+//os=os.withColumn("groupData",callUDF("groupData",col("Sending Country Code")));
+//os.show();
     }
 }
