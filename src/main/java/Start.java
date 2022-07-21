dbf2fb19a736b559e850f9819c65ce02da5739d5
Valcan-Andrada <andrada.valcan01@e-uvt.ro>
2022-07-21 22:09:20 +0300

    .

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,22 +1,32 @@
-import org.apache.hadoop.shaded.org.apache.avro.generic.GenericData;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
 import org.apache.spark.sql.api.java.UDF1;
 import org.apache.spark.sql.types.DataTypes;
-import scala.Array;
+import scala.PartialFunction;
+import scala.collection.immutable.ArraySeq;
 
+
+import java.util.ArrayList;
+
+import
+        scala.collection.Iterator;
+//import scala.collection.immutable.List;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.apache.spark.sql.functions.*;
 
 public class Start {
-    public static void main(String[] args) throws AnalysisException {
+    public static void main(String[] args) {
 
-        System.setProperty("hadoop.home.dir","C:\\BigData\\Hadoop\\bin");
+        System.setProperty("hadoop.home.dir", "C:\\BigData\\Hadoop\\bin");
 
         SparkSession spark = SparkSession
                 .builder()
@@ -24,7 +34,7 @@ public class Start {
                 .master("local[*]")
                 .getOrCreate();
         Dataset<Row> os = spark.read().//csv("src/main/resources/Erasmus.csv");
-                format("csv").option("header","true").load("src/main/resources/Erasmus.csv");
+                format("csv").option("header", "true").load("src/main/resources/Erasmus.csv");
         os.printSchema();
 
         // os.show(20,false);
@@ -48,7 +58,6 @@ public class Start {
         // os.show();
 
 
-
 //       os.filter((os.col("Receiving Country Code").equalTo("RO"))
 //                        .or(os.col("Receiving Country Code").equalTo("AT"))
 //                        .or(os.col("Receiving Country Code").equalTo("BG")))
@@ -58,37 +67,70 @@ public class Start {
         //filtrare 3 coduri de tara care a primit studenti si care au fost codurile
         //de tari din care au provenit studentii si care a fost numarul lor
         //+afisare sortata
-        os.filter((os.col("Receiving Country Code").equalTo("RO"))
-                        .or(os.col("Receiving Country Code").equalTo("LV"))
-                        .or(os.col("Receiving Country Code").equalTo("AT")))
-                        .groupBy(col("Receiving Country Code"),col("Sending Country Code"))
-                        .count().sort("Receiving Country Code","Sending Country Code").show(100);
+        Dataset<Row> udf = os;
+        os =
+                os.filter((os.col("Receiving Country Code").equalTo("RO"))
+                                .or(os.col("Receiving Country Code").equalTo("LV"))
+                                .or(os.col("Receiving Country Code").equalTo("AT")))
+                        .groupBy(col("Receiving Country Code"), col("Sending Country Code"))
+                        .count().sort("Receiving Country Code", "Sending Country Code");
 
+        Properties prop = new Properties();
+        prop.setProperty("user", "postgres");
+        prop.setProperty("password", "dorecuhuso42");
 
-//**********************************
+        os.write().mode(SaveMode.Overwrite).jdbc("jdbc:postgresql://localhost:5432/BigData", "erasmus", prop);
 
-        UDF1<Array<String>,String> groupData = new UDF1<Array<String> ,String>() {
+        Dataset<Row> LV = os.filter(os.col("Receiving Country Code").equalTo("LV"));
+        LV.select(col("Sending Country Code"), col("count")).write().mode(SaveMode.Overwrite).jdbc("jdbc:postgresql://localhost:5432/BigData", "LV", prop);
+        Dataset<Row> RO = os.filter(os.col("Receiving Country Code").equalTo("RO"));
+        RO.select(col("Sending Country Code"), col("count")).write().mode(SaveMode.Overwrite).jdbc("jdbc:postgresql://localhost:5432/BigData", "RO", prop);
+        Dataset<Row> AT = os.filter(os.col("Receiving Country Code").equalTo("AT"));
+        AT.select(col("Sending Country Code"), col("count")).write().mode(SaveMode.Overwrite).jdbc("jdbc:postgresql://localhost:5432/BigData", "AT", prop);
+//****************************************************************************************************************************************************//
+
+        UDF1<scala.collection.Seq, String> groupData = new UDF1<scala.collection.Seq, String>() {
             @Override
-            public String call(Array<String> s) throws Exception {
-                System.out.println("++++++++++++++*+*+*+**+***********************************************************************");
+            public String call(scala.collection.Seq s) throws Exception {
+
+                System.out.println("************************************");
+                List<String> stringList = new ArrayList<>();
+
+
+                Iterator<String> iterator = s.iterator();
+                while (iterator.hasNext()) {
+                    System.out.println(stringList.add(iterator.next()));
+                }
+                //Stream<String> stringStream= stringList.stream();
+
+                System.out.println(stringList);
+                Map<String, Long> dictionar =
+                        stringList.stream().collect(
+                                Collectors.groupingBy(
+                                        Function.identity(),
+                                        HashMap::new,
+                                        Collectors.counting()
+                                )
+                        );
+                dictionar.forEach((key, value) -> System.out.println(key + ":" + value));
+                System.out.println("************************************");
                 //return String.valueOf(s.length());
                 return "da";
             }
         };
 
 
+        spark.udf().register("groupData", groupData, DataTypes.StringType);
 
-        spark.udf().register("groupData",groupData, DataTypes.StringType);
-
-       os.filter((os.col("Receiving Country Code").equalTo("RO"))
-                        .or(os.col("Receiving Country Code").equalTo("LV"))
-                        .or(os.col("Receiving Country Code").equalTo("AT")))
-                .groupBy(col("Receiving Country Code")).agg(collect_set("Sending Country Code")
-                        .as("Sending Country Code")).sort("Sending Country Code").withColumn("groupData",callUDF("groupData",col("Sending Country Code"))).show(100);
-              // .count().sort("Receiving Country Code","Sending Country Code").show(100);
-
-//spark.udf().register("groupData",groupData, DataTypes.StringType);
-//os=os.withColumn("groupData",callUDF("groupData",col("Sending Country Code")));
-//os.show();
+        Dataset<Row> a =
+                udf.filter((os.col("Receiving Country Code").equalTo("RO"))
+                                .or(os.col("Receiving Country Code").equalTo("LV"))
+                                .or(os.col("Receiving Country Code").equalTo("AT")))
+                        .groupBy(col("Receiving Country Code")).agg(collect_list("Sending Country Code")
+                                .as("Sending Country Code")).sort("Sending Country Code").withColumn("groupData", callUDF("groupData", col("Sending Country Code")));
+        // .count().sort("Receiving Country Code","Sending Country Code").show(100);
+//        a.printSchema();
+//        a.show(100);
+        a.write().mode(SaveMode.Overwrite).jdbc("jdbc:postgresql://localhost:5432/BigData", "verificare", prop);
     }
 }
